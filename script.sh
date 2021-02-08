5c5bc26caca3ae3e7b23e6d533c22fc01ed35693
John Tucker <jtucker@spotify.com>
2021-02-07 20:07:04 -0700

    try

--- script.sh
+++ script.sh
@@ -1,6 +1,11 @@
 set -x
 set -e
+echo "### Docker build"
 VERSION=`node -pe "require('./package.json').version"`
-# docker build . -t sckmkny/app-1:$VERSION
-# docker push sckmkny/app-1:$VERSION
+docker build . -t sckmkny/app-1:$VERSION
+echo "### Docker push"
+docker push sckmkny/app-1:$VERSION
+echo "### Kubectl apply"
 sed -i "s/IMAGE/sckmkny\/app-1:$VERSION/" k8s/app-1-01-deployment.yaml
+kubectl apply -f k8s
+
