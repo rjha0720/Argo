809df233bae329c45ffbcd86bf67e84a909c70e1
John Tucker <jtucker@spotify.com>
2021-02-07 19:57:05 -0700

    cp

--- script.sh
+++ script.sh
@@ -1,5 +1,6 @@
 set -x
 set -e
 VERSION=`node -pe "require('./package.json').version"`
-docker build . -t sckmkny/app-1:$VERSION
-docker push sckmkny/app-1:$VERSION
+# docker build . -t sckmkny/app-1:$VERSION
+# docker push sckmkny/app-1:$VERSION
+sed "s/IMAGE/sckmkny\/app-1:$VERSION/" k8s/app-1-01-deployment.yaml
