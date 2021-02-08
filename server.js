6a9a23cd24cb435f86b96b612a68e9aa63de43cc
John Tucker <jtucker@spotify.com>
2021-02-07 21:04:27 -0700

    prometheus

--- server.js
+++ server.js
@@ -2,12 +2,20 @@ const express = require('express');
 const client = require('prom-client');
 
 const PORT = 3000;
-
-const register = new client.Registry();
+const register = client.register;
 client.collectDefaultMetrics({ register });
+const requests  = new client.Counter({
+  name: 'app_1_requests_total',
+  help: 'Current total app-1 requests',
+});
+const notFound = new client.Counter({
+  name: 'app_1_not_founds_total',
+  help: 'Current total app-1 not founds',
+});
 const app = express();
 
 app.get('/', async (req, res) => {
+  requests.inc();
   res.send('Hello World!');
 });
 
@@ -21,7 +29,12 @@ app.get('/metrics', async (req, res) => {
   }
 });
 
+app.use((req, res, next) => {
+  requests.inc();
+  notFound.inc();
+  res.status(404).send("Sorry can't find that!")
+})
+
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
 });
-
