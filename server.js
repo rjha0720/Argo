3515f7ff02ecdb072ca6a634838d7b9391b86510
John Tucker <jtucker@spotify.com>
2021-02-07 20:35:57 -0700

    prom

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const client = require('prom-client');
 
 const PORT = 3000;
 
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
 const app = express();
 
 app.get('/', async (req, res) => {
   res.send('Hello World!');
-})
+});
+
+app.get('/metrics', async (req, res) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    const metrics = await register.metrics();
+    res.send(metrics);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:${PORT}`);
-})
+});
 
