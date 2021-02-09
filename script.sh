5985361fb6ca33d368b8b72691121e592c2f7a0a
John Tucker <jtucker@spotify.com>
2021-02-08 20:30:01 -0700

    fixed

--- script.sh
+++ script.sh
@@ -6,6 +6,6 @@ docker build . -t sckmkny/app-1:$VERSION
 echo "### Docker push"
 docker push sckmkny/app-1:$VERSION
 echo "### Kubectl apply"
-sed -i "s/IMAGE/sckmkny\/app-1:$VERSION/" k8s/app-1-01-deployment.yaml
+sed -i "s/IMAGE/sckmkny\/app-1:$VERSION/" k8s/app-1-01-rollout.yaml
 kubectl apply -f k8s
 
